e614e0d906bca73c57bd7ad7bca7b389ab78f775
*Laxman38* <*Laxmansinghgehloth9206*>
2025-11-09 19:57:10 +0530

    deployment changes

---
+++ middleware/corsConfig.js
@@ -0,0 +1,21 @@
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+    
+        const allowedOrigins = [
+            'http://localhost:5173',    
+            process.env.CORS_ORIGIN
+            ].filter(Boolean);
+
+        if(allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+};
+
+export default corsOptions;
